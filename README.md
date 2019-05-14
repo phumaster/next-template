15ec604611e0b956f450209267886bb78d6a691e
phumaster <pham.ngoc.phu@linecorp.com>
2019-05-14 10:07:44 +0700

    Fix unit test failed

--- README.md
+++ README.md
@@ -1,11 +1,17 @@
 ## NextJs template
 
+> this configuration using 4 environment
+
+1. Alpha: for dev
+2. Beta: for QC test
+3. Development: for local develop
+4. Production: production mode
+
 - [x] Using CSS module
 - [x] Test with Jest
 - [x] Using Typescript
 - [x] Using alias for import modules
 - [x] Using Redux
-- [ ] Fix bug can't not find module CSS when run test
 
 ### Author
 
--- run.sh
+++ run.sh
@@ -9,11 +9,11 @@ cp "./config/$1.env" .env && echo -e "${LIGHT_GREEN}Generate $1.env -> .env file
 if [ $1 = "production" ]; then
 
 elif [ $1 = "alpha" ]; then
-  #npm run alpha && echo "y" | reg alpha && echo -e "${LIGHT_GREEN}[$1] deploy successfully${NC}";
+  # do anything
 elif [ $1 = "beta" ]; then
-  #npm run beta && echo "y" | reg beta && echo -e "${LIGHT_GREEN}[$1] deploy successfully${NC}";
+  # do anything
 elif [ $1 = "development" ]; then
-  #echo -e "You must input a valid parameter ${RED}(real|alpha|beta)${NC}.";
+  # do anything
 else
   echo -e "You must input a valid parameter ${RED}(production|alpha|beta|development)${NC}.";
 fi
